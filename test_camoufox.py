3fe731b806ba7e52340249ea3f8f46c39352e04b
shazam <158528700+anothersidewalklgbt@users.noreply.github.com>
2025-09-16 20:54:31 -0500

    Update test_camoufox.py

--- test_camoufox.py
+++ test_camoufox.py
@@ -9,7 +9,7 @@ from camoufox.sync_api import Camoufox
 # ---------- Config ----------
 TARGET = "https://www.roblox.com/es/CreateAccount"  # URL del formulario de registro
 BASE_PREFIX = "youngceoio"
-NUM_ACCOUNTS = 2000
+NUM_ACCOUNTS = 10
 HEADLESS = True
 CSV_FILE = "accounts.csv"
 PAUSE_BETWEEN = 1
@@ -278,4 +278,5 @@ if __name__ == "__main__":
         print("ATENCIÓN: no has configurado TARGET. Edita el script y pon la URL de tu formulario.")
     ensure_csv_header(CSV_FILE)
     main()
+
 
